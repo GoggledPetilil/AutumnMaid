feafd4a9968bbaad86ba33d30a5671563d847a6d
GoggledPetilil <anakcebu@gmail.com>
2023-01-01 15:51:34 +0100

    v1.20.3.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -94,12 +94,32 @@ public class GameManager : MonoBehaviour
             instance = this;
             m_Anim = GetComponentInChildren<Animator>();
 
+            var options = new Dictionary<string,object>()
+            {
+                // This should match the version number in your Newgrounds App Settings page
+                { "version",            "1.0.0" },
+
+                // If you aren't using any of these features, set them to false, or delete the line
+                //{ "checkHostLicense",   true },
+                //{ "autoLogNewView",     true },
+                //{ "preloadMedals",      true },
+                //{ "preloadScoreBoards", true },
+                //{ "preloadSaveSlots",   true }
+            };
+            // initialize the API, using the App ID and AES key from your Newgrounds project
+            //NGIO.Init(AppID, AesKey, options);
+
             DontDestroyOnLoad(this.gameObject);
         }
         else 
         {
             Destroy(this.gameObject);
         }
+    }
+
+    public void UnlockMedal(int medal_id)
+    {
+        //StartCoroutine(NGIO.UnlockMedal(medal_id));
     }
 
     public void SetCamFollower(GameObject followObj)
