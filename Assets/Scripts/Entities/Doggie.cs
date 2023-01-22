cb4ecfd1adee829ead6356efab15e97f2bfc9257
GoggledPetilil <anakcebu@gmail.com>
2023-01-22 11:20:54 +0100

    v1.28.1

--- Assets/Scripts/Entities/Doggie.cs
+++ Assets/Scripts/Entities/Doggie.cs
@@ -8,7 +8,10 @@ public class Doggie : Entity
     [SerializeField] private Dialogue m_StealDialogue;
     [SerializeField] private SpriteRenderer m_sr;
     [SerializeField] private Sprite[] m_DogSprites;
+    [SerializeField] private Sprite m_DogSadSprites;
     [SerializeField] private Item m_ItemMemento;
+    [SerializeField] private ParticleSystem m_SleepParticles;
+    private bool isTalking;
 
     void Start()
     {
@@ -24,17 +27,37 @@ public class Doggie : Entity
 
     public void TriggerEvent()
     {
+        if(isTalking) return;
         if(GameManager.instance.m_FlagMetGhost && !GameManager.instance.m_FlagHaveMemento)
         {
-            m_sr.sprite = m_DogSprites[1];
-            FindObjectOfType<DialogueManager>().StartDialogue(m_StealDialogue);
-            GameManager.instance.m_FlagHaveMemento = true;
-            GameManager.instance.AddItem(m_ItemMemento);
+            StartCoroutine(DialogueEvent());
         }
         else 
         {
             Player p = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
             p.PatObject(this);
         }
+    }
+
+    IEnumerator DialogueEvent()
+    {
+        isTalking = true;
+        Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        player.StopMovement(true);
+
+        m_sr.sprite = m_DogSadSprites;
+        m_SleepParticles.Stop();
+        FindObjectOfType<DialogueManager>().StartDialogue(m_StealDialogue);
+        GameManager.instance.m_FlagHaveMemento = true;
+        GameManager.instance.AddItem(m_ItemMemento);
+
+        while(GameManager.instance.isTalking())
+        {
+            yield return null;
+        }
+
+        player.StopMovement(false);
+        m_sr.sprite = m_DogSprites[1];
+        isTalking = false;
     }
 }
